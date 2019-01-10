35c55b51ffcf2560ad888f162613eea13e06e958
Philippe Pary <philippe@pachyderme.net>
2019-01-10 10:59:18 +0100

    stopped false latency

--- src/EventListener/ControllerListener.php
+++ src/EventListener/ControllerListener.php
@@ -9,6 +9,6 @@ class ControllerListener
     public function onKernelController(FilterControllerEvent $event)
     {
         // @hack simulation de temps de latence entre 1 et 3 secondes
-        usleep(random_int(1, 3) * 1000000);
+        // usleep(random_int(1, 3) * 1000000);
     }
 }
